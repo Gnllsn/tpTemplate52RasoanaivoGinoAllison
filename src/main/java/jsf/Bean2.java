087a580550b79bac5c084370bfef682412abcde8
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2023-04-04 18:50:21 +0300

    Création des pages des exercices

--- src/main/java/jsf/Bean2.java
+++ src/main/java/jsf/Bean2.java
@@ -29,7 +29,7 @@ public class Bean2 {
     }
 
     public String afficher() {
-        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+        return "affichage__3?nb=" + nombre + "&amp;faces-redirect=true";
     }
 
     public List<Integer> getNombresSuivants() {
